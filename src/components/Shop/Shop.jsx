fc4e0d5618b28a415b40de2a1de9c987d4eccc05
hamed18 <hamedcnec@gmail.com>
2024-09-03 00:34:19 +0600

    Pagination: count no. of items and item per page. Created dynamic button

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,11 +3,24 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+    // Pagination: 
+    // total no of items. items per page
+    const {count} = useLoaderData();
+  // console.log(count);
+    const itemsPerPage = 10;
+    const numberOfPages = Math.ceil(count / itemsPerPage);
+
+    const pages = []
+    for (let i=0; i<numberOfPages; i++){
+        pages.push(i);
+    }
+ //   const pages = [...Array(numberOfPages).keys()];
+    console.log(pages);
 
     useEffect(() => {
         fetch('http://localhost:4000/products')
@@ -81,6 +94,16 @@ const Shop = () => {
                         <button className='btn-proceed'>Review Order</button>
                     </Link>
                 </Cart>
+            </div>
+            <div className="pagination">
+                {
+                    pages.map(page =>
+                        <button
+                            key = {page}>
+                                {page}
+                        </button>
+                    )
+                }
             </div>
         </div>
     );
